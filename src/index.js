f6550c6e100dcc65eb37cac374da357a985d3029
merchanha <merchanha@gmail.com>
2022-04-07 17:05:13 -0300

    socket.io connection

--- src/index.js
+++ src/index.js
@@ -1,14 +1,23 @@
 const express = require('express')
 const path = require('path')
+const http =require('http')
+const socketio = require('socket.io')
 
+
+const app = express()
+const server = http.createServer(app)
+const io = socketio(server)
 
 const port = process.env.PORT || 3000
-const app = express()
 const publicDirectoryPath = path.join(__dirname, '../public')
 
 
 app.use(express.static(publicDirectoryPath))
 
-app.listen(3000, ()=>{
+io.on('connection', ()=>{
+    console.log('New Websocket connection')
+})
+
+server.listen(3000, ()=>{
     console.log(`Server is up on port ${port}`)
 })
